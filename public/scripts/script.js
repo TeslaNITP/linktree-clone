9471658ef81289bdb8440c62c72cca4d534330ff
SudhanshuuRanjan <sudhanshur.ug20.ee@nitp.ac.in>
2022-02-18 16:24:35 +0530

    added navbar

---
+++ public/scripts/script.js
@@ -0,0 +1,13 @@
+function copytoclipboard() {
+  var text = document.querySelector(".copy").getAttribute("id");
+  navigator.clipboard.writeText(text).then(
+    function () {
+      console.log("Async: Copying to clipboard was successful!");
+    },
+    function (err) {
+      console.error("Async: Could not copy text: ", err);
+    }
+  );
+}
+
+
